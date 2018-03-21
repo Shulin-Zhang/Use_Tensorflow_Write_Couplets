2d1c7f92f772acb632f1d73b3f52993d5bf1f4b4
zhangshulin <zhangslwork@yeah.net>
2018-03-21 11:14:29 +0800

    可以写对联了

--- couplets_utils.py
+++ couplets_utils.py
@@ -48,17 +48,34 @@ def load_sample_datasets(vocabs_size=5000, max_len=30, batch_size=16, sample_siz
     }
 
 
+def convert_predict_to_text(predict, index2word):
+    sequence = np.argmax(predict, axis=-1)
+    predict[sequence == 1] = 0
+    result_seq = np.argmax(predict, axis=-1)
+
+    return convert_sequence_to_text(result_seq, index2word)
+
+
 def convert_sequence_to_text(sequence, index2word):
-    text_arr = [index2word[int(index)] for index in sequence]
+    text_arr = [index2word[int(index)] for index in np.squeeze(sequence)]
 
     return ''.join(text_arr)
+    
 
+def convert_text_to_onehot(text, vocabs_size, max_len, word2index):
+    onehot = np.zeros((1, max_len, vocabs_size))
+    for pos, word in enumerate(text.strip()):
+        index = word2index[word]
+        onehot[0, pos + 1, index] = 1
+
+    return onehot
 
 
 # TEST
 if __name__ == '__main__':
     result = load_sample_datasets()
     generator = result['sample_gen']
+    word2index = result['word2index']
 
     num = 0
     for x, y in generator:
@@ -66,5 +83,9 @@ if __name__ == '__main__':
         num += 1
         if num == 20:
             break
+
+    text = '床前明月光'
+    onehot = convert_text_to_onehot(text, 5000, 30, word2index)
+    print('onehot:', onehot.shape)
             
 
