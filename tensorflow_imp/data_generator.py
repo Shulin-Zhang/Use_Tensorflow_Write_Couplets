a4de74a4040198a17715d1559007fef99f4e14c1
zhangshulin <zhangslwork@yeah.net>
2018-04-18 16:24:04 +0800

    helper batch generator

--- tensorflow_imp/data_generator.py
+++ tensorflow_imp/data_generator.py
@@ -6,5 +6,53 @@
 
 
 import tensorflow as tf
+import numpy as np
+import helper
 
 
+class CoupletsDataGenerator:
+
+    def __init__(self, set_array, shuffle=True, buffer_size=10000):
+        self._data = tf.data.Dataset.from_tensor_slices(set_array)
+        
+        if shuffle:
+            self._data = self._data.shuffle(buffer_size=buffer_size)
+
+
+    def get_batch(self, session, batch_size):
+        self._data = self._data.batch(batch_size)
+        
+        iterator = self._data.make_one_shot_iterator()
+        next = iterator.get_next()
+
+        while True:
+            try:
+                data_set = session.run(next)
+                Y = data_set
+                fore_zeros = np.zeros((data_set.shape[0], 1), dtype=np.int32)
+                X = np.concatenate((fore_zeros, Y[:, : -1]), axis=1)
+
+                yield X, Y
+
+            except tf.errors.OutOfRangeError:
+                break
+
+
+def test():
+    import helper 
+
+    _, _, _, _, _, test_set = helper.process_dataset()
+    data_g = CoupletsDataGenerator(test_set)
+    sess = tf.Session()
+    batch_g = data_g.get_batch(sess, 2)
+
+    next_data = next(batch_g)
+
+    print('X shape:', next_data[0].shape)
+    print('Y shape:', next_data[1].shape)
+    print(next_data[0][1, :5])
+    print(next_data[1][1, :5])
+
+
+if __name__ == '__main__':
+    test()
--- tensorflow_imp/helper.py
+++ tensorflow_imp/helper.py
@@ -10,10 +10,14 @@ CUT_PATH = './datasets/all_cut_couplets.txt'
 TRAIN_PATH = './datasets/train_couplets.txt'
 DEV_PATH = './datasets/dev_couplets.txt'
 TEST_PATH = './datasets/test_couplets.txt'
+TRAIN_SET = './datasets/train_set.npy'
+DEV_SET = './datasets/dev_set.npy'
+TEST_SET = './datasets/test_set.npy'
 
 
 import jieba
 from collections import Counter
+import numpy as np
 import os
 
 
@@ -34,11 +38,11 @@ def create_vocab(input_path=CUT_PATH):
     with open(input_path, encoding='utf-8') as f:
         text = f.read()
 
-    words_counter = Counter(text)
-    words_sorted = sorted(words_counter, key=words_counter.get, reverse=True)
+    words_counter = Counter(text.split())
+    words_sorted = [' '] + sorted(words_counter, key=words_counter.get, reverse=True)
 
-    index2word = list(enumerate(words_sorted))
-    word2index = {word: index for index, word in index2word}
+    index2word = dict(enumerate(words_sorted))
+    word2index = {word: index for index, word in index2word.items()}
     vocab_size = len(words_sorted)
 
     return vocab_size, index2word, word2index
@@ -65,6 +69,68 @@ def divide_dataset(all_path=CUT_PATH, train_path=TRAIN_PATH, dev_path=DEV_PATH, 
     test_file.close()
 
 
+def convert_text_line_to_ints(text_line, word2index):
+    words_list = text_line.split()
+    int_list = [word2index[word] for word in words_list]
+
+    return int_list
+
+
+def convert_data_file(file_path, word2index):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        text_lines_list = f.readlines()
+    
+    int_lines_list = [convert_text_line_to_ints(line, word2index) for line in text_lines_list]
+    
+    max_length = 0
+    for int_line in int_lines_list:
+        if len(int_line) > max_length:
+            max_length = len(int_line)
+
+    padding_array = np.zeros((len(int_lines_list), max_length), dtype=np.int32)
+    for index, int_line in enumerate(int_lines_list):
+        padding_array[index, :len(int_line)] = int_line
+    
+    return padding_array
+
+
+def process_dataset(all_couplets=DATA_PATH):
+    if not os.path.exists(CUT_PATH):
+        cut_dataset()
+
+    vocab_size, index2word, word2index = create_vocab()
+
+    if os.path.exists(TRAIN_SET) and os.path.exists(DEV_SET) and os.path.exists(TEST_SET):
+        train_set = np.load(TRAIN_SET)
+        dev_set = np.load(DEV_SET)
+        test_set = np.load(TEST_SET)
+
+        print('vocab_size:', vocab_size)
+        print('train_set shape:', train_set.shape)
+        print('dev_set shape:', dev_set.shape)
+        print('test_set shape:', test_set.shape)
+
+        return vocab_size, index2word, word2index, train_set, dev_set, test_set
+
+    if (not os.path.exists(TRAIN_PATH)) or (not os.path.exists(DEV_PATH)) or (not os.path.exists(TEST_PATH)):
+        divide_dataset()
+    
+    train_set = convert_data_file(TRAIN_PATH, word2index)
+    dev_set = convert_data_file(DEV_PATH, word2index)
+    test_set = convert_data_file(TEST_PATH, word2index)
+
+    np.save(TRAIN_SET, train_set)
+    np.save(DEV_SET, dev_set)
+    np.save(TEST_SET, test_set)
+
+    print('vocab_size:', vocab_size)
+    print('train_set shape:', train_set.shape)
+    print('dev_set shape:', dev_set.shape)
+    print('test_set shape:', test_set.shape)
+
+    return vocab_size, index2word, word2index, train_set, dev_set, test_set
+
+
 def test_cut_dataset():
     cut_dataset(input_path=DATA_PATH, output_path=CUT_PATH)
 
@@ -73,8 +139,13 @@ def test_divide_dataset():
     divide_dataset()
 
 
+def test_process_dataset():
+    process_dataset()
+
+
 if __name__ == '__main__':
     # test_cut_dataset()
-    test_divide_dataset()
+    # test_divide_dataset()
+    test_process_dataset()
 
 
